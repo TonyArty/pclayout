f6ef3ee2bfb5d6eb35e457b59737b02f427c2424
Anton K <i.openuser@yandex.ru>
2025-03-26 17:37:10 +0500

    comp_js-plus

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const contents = document.querySelectorAll('.program-line__content')
+
+contents.forEach((elem) => {
+	const title = elem.querySelector('.program-line__title')
+	const descr = elem.querySelector('.program-line__descr')
+	const descr_all = elem.querySelectorAll('.program-line__descr')
+
+
+	title.addEventListener('click', () => {
+
+		descr.classList.toggle('active');
+
+	})
+
+})
+
