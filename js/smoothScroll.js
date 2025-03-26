783fb147fcd6723abf9e50de76d9caf6ba6b5509
Anton K <i.openuser@yandex.ru>
2025-03-26 20:44:14 +0500

    adding CDN seamless-scroll-polyfill js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,2 +1,16 @@
-const nav = document.querySelector('.header__nav')
-console.log(nav);
+const navBar = document.querySelector('.header__nav')
+const navLinks = navBar.querySelectorAll('a')
+
+navLinks.forEach((link) => {
+	link.addEventListener('click', (event) => {
+		event.preventDefault()
+		const section = document.querySelector(link.getAttribute('href'))
+
+		if (section) {
+			section.scrollIntoView({
+				block: 'center',
+				behavior: 'smooth'
+			})
+		}
+	})
+})
