ffc3c6106ca395feb1eb05475d961c98e09256e8
Anton K <i.openuser@yandex.ru>
2025-03-26 21:09:20 +0500

    open and close the course order popup

--- js/modal.js
+++ js/modal.js
@@ -20,3 +20,28 @@ modalInner.append(closeBtn);
 closeBtn.addEventListener('click', () => {
 	modal.style.display = 'none'
 })
+
+
+
+const courseBtn = document.querySelector('.course__button')
+const modalCourse = document.querySelector('#course')
+
+courseBtn.addEventListener('click', () => {
+	modalCourse.style.display = 'flex'
+})
+
+modalCourse.addEventListener('click', (event) => {
+	const modalContentCourse = event.target.closest('.modal__inner')
+	if (!modalContentCourse) {
+		modalCourse.style.display = 'none'
+	}
+})
+
+const modalInnerCourse = modalCourse.querySelector('.modal__inner');
+modalInnerCourse.style = 'position: relative;'
+const closeBtnCourse = document.createElement('div');
+closeBtnCourse.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
+modalInnerCourse.append(closeBtnCourse);
+closeBtnCourse.addEventListener('click', () => {
+	modalCourse.style.display = 'none'
+})
