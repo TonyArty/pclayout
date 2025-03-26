3f037fb80f62a3a52cf1d7652e5f808f1507e176
Anton K <i.openuser@yandex.ru>
2025-03-26 20:18:16 +0500

    modal open close is done

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,18 @@ modalBtn.addEventListener('click', () => {
 	modal.style.display = 'flex'
 })
 
-modal.addEventListener('click', () => {
-	console.log('modal');
+modal.addEventListener('click', (event) => {
+	const modalContent = event.target.closest('.modal__inner')
+	if (!modalContent) {
+		modal.style.display = 'none'
+	}
+})
+
+const modalInner = document.querySelector('.modal__inner');
+modalInner.style = 'position: relative;'
+const closeBtn = document.createElement('div');
+closeBtn.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
+modalInner.append(closeBtn);
+closeBtn.addEventListener('click', () => {
+	modal.style.display = 'none'
 })
