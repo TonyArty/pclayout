ce92c0afdf52050537c240f24fa0f34dc49abaa6
Anton K <i.openuser@yandex.ru>
2025-03-26 20:51:38 +0500

    seamless.scrollIntoView

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -5,12 +5,12 @@ navLinks.forEach((link) => {
 	link.addEventListener('click', (event) => {
 		event.preventDefault()
 		const section = document.querySelector(link.getAttribute('href'))
-
 		if (section) {
-			section.scrollIntoView({
-				block: 'center',
-				behavior: 'smooth'
-			})
+			seamless.scrollIntoView(section, {
+				behavior: "smooth",
+				block: "start",
+				inline: "center",
+			});
 		}
 	})
 })
