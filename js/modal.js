9c7b23b5011d7365d2674cbeebbb8c4482c0b811
Anton K <i.openuser@yandex.ru>
2025-03-27 23:00:09 +0500

    commenting modal course

--- js/modal.js
+++ js/modal.js
@@ -26,27 +26,28 @@ const modal = () => {
 	
 	// popup about Course
 	
-	const courseBtn = document.querySelector('.course__button')
-	const modalCourse = document.querySelector('#course')
+	// const courseBtn = document.querySelector('.course__button')
+	// const modalCourse = document.querySelector('#course')
 	
-	courseBtn.addEventListener('click', () => {
-		modalCourse.style.display = 'flex'
-	})
+	// courseBtn.addEventListener('click', () => {
+	// 	modalCourse.style.display = 'flex'
+	// })
 	
-	modalCourse.addEventListener('click', (event) => {
-		const modalContentCourse = event.target.closest('.modal__inner')
-		if (!modalContentCourse) {
-			modalCourse.style.display = 'none'
-		}
-	})
+	// modalCourse.addEventListener('click', (event) => {
+	// 	const modalContentCourse = event.target.closest('.modal__inner')
+	// 	if (!modalContentCourse) {
+	// 		modalCourse.style.display = 'none'
+	// 	}
+	// })
 	
-	const modalInnerCourse = modalCourse.querySelector('.modal__inner');
-	modalInnerCourse.style = 'position: relative;'
-	const closeBtnCourse = document.createElement('div');
-	closeBtnCourse.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
-	modalInnerCourse.append(closeBtnCourse);
-	closeBtnCourse.addEventListener('click', () => {
-		modalCourse.style.display = 'none'
-	})
+	// const modalInnerCourse = modalCourse.querySelector('.modal__inner');
+	// modalInnerCourse.style = 'position: relative;'
+	// const closeBtnCourse = document.createElement('div');
+	// closeBtnCourse.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
+	// modalInnerCourse.append(closeBtnCourse);
+	// closeBtnCourse.addEventListener('click', () => {
+	// 	modalCourse.style.display = 'none'
+	// })
+
 }
 modal()
