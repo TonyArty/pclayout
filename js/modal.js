ce8294872ac9d98e49099edaf47abb614c4d733f
Anton K <i.openuser@yandex.ru>
2025-03-27 11:18:06 +0500

    encapsulation

--- js/modal.js
+++ js/modal.js
@@ -1,47 +1,52 @@
-const modalBtn = document.querySelector('.modal__button')
-const modal = document.querySelector('.modal')
+const modal = () => {
+	// popup about PC
 
-modalBtn.addEventListener('click', () => {
-	modal.style.display = 'flex'
-})
-
-modal.addEventListener('click', (event) => {
-	const modalContent = event.target.closest('.modal__inner')
-	if (!modalContent) {
+	const modalBtn = document.querySelector('.modal__button')
+	const modal = document.querySelector('.modal')
+	
+	modalBtn.addEventListener('click', () => {
+		modal.style.display = 'flex'
+	})
+	
+	modal.addEventListener('click', (event) => {
+		const modalContent = event.target.closest('.modal__inner')
+		if (!modalContent) {
+			modal.style.display = 'none'
+		}
+	})
+	
+	const modalInner = document.querySelector('.modal__inner');
+	modalInner.style = 'position: relative;'
+	const closeBtn = document.createElement('div');
+	closeBtn.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
+	modalInner.append(closeBtn);
+	closeBtn.addEventListener('click', () => {
 		modal.style.display = 'none'
-	}
-})
-
-const modalInner = document.querySelector('.modal__inner');
-modalInner.style = 'position: relative;'
-const closeBtn = document.createElement('div');
-closeBtn.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
-modalInner.append(closeBtn);
-closeBtn.addEventListener('click', () => {
-	modal.style.display = 'none'
-})
-
-
-
-const courseBtn = document.querySelector('.course__button')
-const modalCourse = document.querySelector('#course')
-
-courseBtn.addEventListener('click', () => {
-	modalCourse.style.display = 'flex'
-})
-
-modalCourse.addEventListener('click', (event) => {
-	const modalContentCourse = event.target.closest('.modal__inner')
-	if (!modalContentCourse) {
+	})
+	
+	// popup about Course
+	
+	const courseBtn = document.querySelector('.course__button')
+	const modalCourse = document.querySelector('#course')
+	
+	courseBtn.addEventListener('click', () => {
+		modalCourse.style.display = 'flex'
+	})
+	
+	modalCourse.addEventListener('click', (event) => {
+		const modalContentCourse = event.target.closest('.modal__inner')
+		if (!modalContentCourse) {
+			modalCourse.style.display = 'none'
+		}
+	})
+	
+	const modalInnerCourse = modalCourse.querySelector('.modal__inner');
+	modalInnerCourse.style = 'position: relative;'
+	const closeBtnCourse = document.createElement('div');
+	closeBtnCourse.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
+	modalInnerCourse.append(closeBtnCourse);
+	closeBtnCourse.addEventListener('click', () => {
 		modalCourse.style.display = 'none'
-	}
-})
-
-const modalInnerCourse = modalCourse.querySelector('.modal__inner');
-modalInnerCourse.style = 'position: relative;'
-const closeBtnCourse = document.createElement('div');
-closeBtnCourse.innerHTML = '<a style="position: absolute; top: 20px; right: 20px; font-size: 30px; font-weight: bold; cursor: pointer;">&#x2715;</a>';
-modalInnerCourse.append(closeBtnCourse);
-closeBtnCourse.addEventListener('click', () => {
-	modalCourse.style.display = 'none'
-})
+	})
+}
+modal()
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,16 +1,19 @@
-const navBar = document.querySelector('.header__nav')
-const navLinks = navBar.querySelectorAll('a')
-
-navLinks.forEach((link) => {
-	link.addEventListener('click', (event) => {
-		event.preventDefault()
-		const section = document.querySelector(link.getAttribute('href'))
-		if (section) {
-			seamless.scrollIntoView(section, {
-				behavior: "smooth",
-				block: "start",
-				inline: "center",
-			});
-		}
+const smoothScroll = () => {
+	const navBar = document.querySelector('.header__nav')
+	const navLinks = navBar.querySelectorAll('a')
+	
+	navLinks.forEach((link) => {
+		link.addEventListener('click', (event) => {
+			event.preventDefault()
+			const section = document.querySelector(link.getAttribute('href'))
+			if (section) {
+				seamless.scrollIntoView(section, {
+					behavior: "smooth",
+					block: "start",
+					inline: "center",
+				});
+			}
+		})
 	})
-})
+}
+smoothScroll()
