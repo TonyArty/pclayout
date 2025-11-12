862aa19cdbe2313b50fa71fab6a6754cd1346b84
Anton K <i.openuser@yandex.ru>
2025-11-13 00:09:30 +0500

    fetch new

--- js/sendForm.js
+++ js/sendForm.js
@@ -13,19 +13,32 @@ const sendForm = () => {
 			email: email.value
 		}
 
-		fetch('https://jsonplaceholder.typicode.com/todos/454367')
-		.then(response => {
-			if (response.status === 404) {
-				throw new Error('Not found!')
-			}
-			return response.json()
+		// fetch('https://jsonplaceholder.typicode.com/todos/454367')
+		// .then(response => {
+		// 	if (response.status === 404) {
+		// 		throw new Error('Not found!')
+		// 	}
+		// 	return response.json()
+		// })
+		// .then(data => {
+		// 	console.log(data);
+		// })
+		// .catch(error => {
+		// 	console.warn(error.message);
+		// })
+
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			body: JSON.stringify(sendObj),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
 		})
-		.then(data => {
-			console.log(data);
-		})
-		.catch(error => {
-			console.warn(error.message);
-		})
+		.then((response) => response.json())
+		.then((json) => console.log(json))
+		.finally(() => {
+			console.log('Форма очищена')
+		});
 	})
 }
 sendForm()
